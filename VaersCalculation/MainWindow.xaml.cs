1b18c7c28a0d337035d0e1de7ef74cd9ef22a1c3
hexagod <hexagod@protonmail.com>
2021-06-03 04:38:15 -0400

    swiitch to vaers record class

--- VaersCalculation/MainWindow.xaml.cs
+++ VaersCalculation/MainWindow.xaml.cs
@@ -28,36 +28,131 @@ namespace VaersCalculation
 
         List<string> AllFileLines = new List<string>();
 
+        public class VaersRecord
+        {
+            public string PreviousLineDate { get; set; }
+            public string LineDate { get; set; }
+            public int Id { get; set; }
+            public int TotalSkippedLines { get; set; }
+            public int SkippedLinesAtThisId { get; set; }
+            public int LineId { get; set; }
+        }
+
         private void ReadFileButton_Click(object sender, RoutedEventArgs e)
         {
-            PerformLineCalculationDiff(FileSourceTextBox.Text);
+            ClearUiValues();
+            PerformLineCalculationDiff(FileSourceTextBox.Text, (bool)GenerateFullIdReportCheckBox.IsChecked);
         }
-
-        public async void PerformLineCalculationDiff(string filePath)
+        
+        public async void PerformLineCalculationDiff(string filePath, bool generateReport = false)
         {
-            var ids = await ReadFileContents(filePath);
+            var ids = new List<int>();
+            var vaersLines = new List<string>();
             var totalSkippedLines = 0;
-            var previousLineId = ids.First();
             var outputText = "";
+            var outputReport = "";
+            if (!generateReport)
+            {
+                ids = await ReadFileContentsAsInts(filePath);
+            }
+            else
+            {
+                vaersLines = await ReadFileContentsAsStrings(filePath);
+            }
             await Task.Factory.StartNew(() =>
             {
-                foreach (var lineId in ids)
+                if (!generateReport)
                 {
-                    var lineDiff = lineId - previousLineId;
-                    if (lineDiff > 1)
+                    var previousLineId = ids.First();
+                    foreach (var lineId in ids)
                     {
-                        totalSkippedLines += lineDiff - 1;
-                        outputText += $"<<<<<<<<<<<< {lineDiff - 1} Skipped Lines detected of total {totalSkippedLines}!!!!>>>>>>>>>" + "\n";
+                        var lineDiff = lineId - previousLineId;
+                        if (lineDiff > 1)
+                        {
+                            totalSkippedLines += lineDiff - 1;
+                            outputText += $"<<<<<<<<<<<< {lineDiff - 1} Skipped Lines detected of total {totalSkippedLines}!!!!>>>>>>>>>" + "\n";
+                        }
+                        outputText += lineId.ToString() + "\n";
+                        previousLineId = lineId;
                     }
-                    outputText += lineId.ToString() + "\n";
-                    previousLineId = lineId;
+                }
+                else
+                {
+                    var previousLineId = Convert.ToInt32(vaersLines.First().Split(',')[0]);
+                    var previousLineDate = "";
+                    var recordId = Convert.ToInt32(vaersLines.First().Split(',')[0]);
+                    var lineDate = "";
+                    //var missingDatePairsById = new List<Tuple<int, string, string, int, int>>();
+                    var missingDatePairsById = new List<VaersRecord>();
+                    var lineNumber = 0;
+                    foreach (var vaersLine in vaersLines)
+                    {
+                        recordId = Convert.ToInt32(vaersLine.Split(',')[0]);
+                        var lineDiff = Convert.ToInt32(recordId) - Convert.ToInt32(previousLineId);
+                        lineDate = vaersLine.Split(',')[1];
+                        if (lineDiff > 1)
+                        {
+                            totalSkippedLines += lineDiff - 1;
+                            outputText += $"<<<<<<<<<< {lineDiff - 1} Skipped, total {totalSkippedLines}, range {previousLineDate}=>{lineDate}>>>>>>>>>" + "\n";
+                            missingDatePairsById.Add(new VaersRecord
+                            {
+                                Id = recordId,
+                                PreviousLineDate = previousLineDate,
+                                LineDate = lineDate,
+                                TotalSkippedLines = totalSkippedLines,
+                                SkippedLinesAtThisId = lineDiff - 1,
+                                LineId = lineNumber
+                            });
+                            //missingDatePairsById.Add(new Tuple<int, string, string, int, int>(lineId, previousLineDate, lineDate, totalSkippedLines,(lineDiff-1)));
+                        }
+                        lineNumber++;
+                        outputText += recordId.ToString() + " | " + lineDate + $" | line #: {lineNumber}" + "\n";
+                        previousLineDate = lineDate;
+                        previousLineId = recordId;
+                    }
+                    outputReport = GenerateOutputReport(missingDatePairsById, totalSkippedLines, lineNumber);
                 }
             });
-            TotalSkippedIdsTextBox.Text = totalSkippedLines.ToString();
-            ContentsTextBox.Text = outputText;
+            OutputResultsToUi(totalSkippedLines, outputText, outputReport);
         }
 
-        public Task<List<int>> ReadFileContents(string filePath)
+        public static string GenerateOutputReport(List<VaersRecord> records, int totalSkippedLines, int totalLines)
+        {
+            string outputReport = "";
+            var startDate = records[0].PreviousLineDate;
+            var endDate = records.Last().LineDate;
+            var startId = records[0].Id;
+            var endId = records.Last().Id;
+            outputReport = $"Summary: " + "\n" + $"A total of {totalSkippedLines} skipped IDs detected." + "\n" +
+                $"Id/Date range = {startId} ({startDate}) => {endId} ({endDate})" + "\n" +
+                $"which makes for a total of {endId - startId} IDs in range. " + "\n" +
+                $"the total percentage of skipped Ids relative to lines is: " +
+                $"{((double)totalSkippedLines/(double)totalLines) * 100}%" + "\n" + 
+                $"==========begin detail============" + "\n";
+            
+            foreach (var record in records)
+            {
+                outputReport += $"{record.SkippedLinesAtThisId} Missing IDs@ {record.Id} from {record.PreviousLineDate} => {record.LineDate} of total {record.TotalSkippedLines}" + "\n";
+            }
+            return outputReport;
+        }
+
+        public void ClearUiValues()
+        {
+            OutputResultsToUi(0, " ", " ");
+        }
+
+        public void OutputResultsToUi(int totalSkippedLines, string aggregateTextOutput, string outputReport = "")
+        {
+            TotalSkippedIdsTextBox.Text = totalSkippedLines.ToString();
+            ContentsTextBox.Text = aggregateTextOutput;
+            if (outputReport != "")
+            {
+                ReportTextBox.Text = outputReport;
+            }
+        }
+
+        public Task<List<int>> ReadFileContentsAsInts(string filePath)
         {
             StatusTextBox.Text = $"reading file path {filePath}";
             if (AllFileLines.Count > 0)
@@ -71,10 +166,27 @@ namespace VaersCalculation
             {
                 var lineIdInt = Convert.ToInt32(AllFileLines[i].Split(',')[0]);
                 AllVAERSIdNumbers.Add(lineIdInt);
-
             }
-            AllFileLines.Sort();
+            AllVAERSIdNumbers.Sort();
             return Task.FromResult(AllVAERSIdNumbers);
+        }
+
+        public Task<List<string>> ReadFileContentsAsStrings(string filePath)
+        {
+            StatusTextBox.Text = $"reading file path {filePath}";
+            if (AllFileLines.Count > 0)
+            {
+                AllFileLines = new List<string>();
+            }
+            AllFileLines = System.IO.File.ReadAllLines(filePath).ToList();
+            AllFileLines.RemoveAt(0);
+            ContentsTextBox.Text = "";
+            var ordered = AllFileLines.Select(s => new { Str = s, Split = s.Split(',') })
+                .OrderBy(x => int.Parse(x.Split[0]))
+                //.ThenBy(x => x.Split[1])
+                .Select(x => x.Str)
+                .ToList();
+            return Task.FromResult(ordered);
         }
 
         private void VAERSCsvFileChooser_Click(object sender, RoutedEventArgs e)
